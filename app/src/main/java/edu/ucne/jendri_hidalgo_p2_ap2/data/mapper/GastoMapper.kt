0c152e1863e228a509d0bf05d08491daf81bd4f4
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-06 02:08:48 -0400

    Se agregó lo que faltaba.

--- app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/data/mapper/GastoMapper.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/data/mapper/GastoMapper.kt
@@ -1,2 +1,26 @@
 package edu.ucne.jendri_hidalgo_p2_ap2.data.mapper
 
+import edu.ucne.jendri_hidalgo_p2_ap2.data.remote.Dto.GastoDto
+import edu.ucne.jendri_hidalgo_p2_ap2.domain.model.Gasto
+
+fun GastoDto.toGasto(): Gasto {
+    return Gasto(
+        gastoId = gastoId,
+        fecha = fecha,
+        suplidor = suplidor,
+        ncf = ncf,
+        itbis = itbis,
+        monto = monto
+    )
+}
+
+fun Gasto.toDto(): GastoDto {
+    return GastoDto(
+        gastoId = gastoId,
+        fecha = fecha,
+        suplidor = suplidor,
+        ncf = ncf,
+        itbis = itbis,
+        monto = monto
+    )
+}
