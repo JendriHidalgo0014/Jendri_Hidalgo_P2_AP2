c51a7774b43572ec2eb9157c83fa4ecb020bb3b4
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-05 21:26:20 -0400

    Se agregó el contenido de ApiService.

--- app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/data/remote/ApiService.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/data/remote/ApiService.kt
@@ -1,4 +1,27 @@
 package edu.ucne.jendri_hidalgo_p2_ap2.data.remote
 
+import edu.ucne.jendri_hidalgo_p2_ap2.data.remote.Dto.GastoDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
 interface ApiService {
+    @GET("api/Gastos")
+    suspend fun getGasto(): Response<List<GastoDto>>
+
+    @GET("api/Gastos/{id}")
+    suspend fun getGasto(@Path("id") id: Int): Response<GastoDto>
+
+    @POST("api/Gastos")
+    suspend fun createGasto(@Body gasto: GastoDto): Response<GastoDto>
+
+    @PUT("api/Gastos/{id}")
+    suspend fun updateGasto(
+        @Path("id") id: Int,
+        @Body gasto: GastoDto
+    ): Response<GastoDto>
+
 }
