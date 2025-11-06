a4196dfcd7db6521182b9e05e8be01526f2e2c17
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-05 20:21:38 -0400

    Se agregó el contenido de ApiModule.

--- app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/di/ApiModule.kt
@@ -1,7 +1,37 @@
 package edu.ucne.jendri_hidalgo_p2_ap2.di
 
+import android.health.connect.datatypes.MealType
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+import kotlin.jvm.java
 
+@InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
+
+    const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net/api/Gastos/"
+
+    @Singleton
+    @Provides
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
 }
