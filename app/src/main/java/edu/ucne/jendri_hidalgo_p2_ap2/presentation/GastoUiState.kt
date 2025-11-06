333c19de61aa3879a7c78fdcf8ff5177877fa46e
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-06 02:09:13 -0400

    Se agregó lo que faltaba.

--- app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/presentation/GastoUiState.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/presentation/GastoUiState.kt
@@ -1,2 +1,18 @@
 package edu.ucne.jendri_hidalgo_p2_ap2.presentation
 
+import edu.ucne.jendri_hidalgo_p2_ap2.domain.model.Gasto
+
+data class GastoUiState(
+    val isLoading: Boolean = false,
+    val gasto: List<Gasto> = emptyList(),
+    val error: String? = null,
+    val gastoSeleccionado: Gasto? = null,
+    val fecha: String = "",
+    val suplidor: String = "",
+    val ncf: String = "",
+    val itbis: String = "",
+    val monto: String = "",
+    val showBottomSheet: Boolean = false,
+    val isEditing: Boolean = false,
+    val successMessage: String? = null
+)
