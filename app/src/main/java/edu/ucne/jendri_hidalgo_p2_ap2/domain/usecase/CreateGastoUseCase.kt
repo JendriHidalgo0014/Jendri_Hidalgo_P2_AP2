6b07d786170fdc758be4fc16650cfe7f4ce049ba
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-06 02:08:37 -0400

    Se agregó lo que faltaba.

--- app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/domain/usecase/CreateGastoUseCase.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/domain/usecase/CreateGastoUseCase.kt
@@ -1,2 +1,15 @@
 package edu.ucne.jendri_hidalgo_p2_ap2.domain.usecase
 
+import edu.ucne.jendri_hidalgo_p2_ap2.data.remote.Resource
+import edu.ucne.jendri_hidalgo_p2_ap2.domain.model.Gasto
+import edu.ucne.jendri_hidalgo_p2_ap2.domain.repository.GastoRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CreateGastoUseCase @Inject constructor(
+    private val repository: GastoRepository
+) {
+    operator fun invoke(gasto: Gasto): Flow<Resource<Gasto>> {
+        return repository.createGasto(gasto)
+    }
+}
