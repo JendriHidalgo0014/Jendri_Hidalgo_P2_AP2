bd2b234173dd9d244160f2111965990fb46ef612
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-05 20:48:16 -0400

    Se agrego el contenido de Dto.

--- app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/data/remote/Dto/GastoDto.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/data/remote/Dto/GastoDto.kt
@@ -1,4 +1,11 @@
 package edu.ucne.jendri_hidalgo_p2_ap2.data.remote.Dto
 
-class GastoDto {
-}
+class GastoDto (
+
+    val gastoId: Int,
+    val fecha: String,
+    val suplidor: String,
+    val ncf: String,
+    val itbis: Double,
+    val monto: Double
+)
