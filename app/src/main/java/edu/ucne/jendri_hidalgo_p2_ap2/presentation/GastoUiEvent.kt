700add06183c840092e6e93911123d4ceaf33f8b
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-11-06 02:09:10 -0400

    Se agregó lo que faltaba.

--- app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/presentation/GastoUiEvent.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_p2_ap2/presentation/GastoUiEvent.kt
@@ -1,2 +1,17 @@
 package edu.ucne.jendri_hidalgo_p2_ap2.presentation
 
+import edu.ucne.jendri_hidalgo_p2_ap2.domain.model.Gasto
+
+sealed interface GastoEvent {
+    data object GetGasto: GastoEvent
+    data class FechaChanged(val fecha: String) : GastoEvent
+    data class SuplidorChanged(val suplidor: String) : GastoEvent
+    data class NcfChanged(val ncf: String) : GastoEvent
+    data class ItbisChanged(val itbis: String) : GastoEvent
+    data class MontoChanged(val monto: String) : GastoEvent
+    data object OpenBottomSheet : GastoEvent
+    data object CloseBottomSheet : GastoEvent
+    data object Save : GastoEvent
+    data class Edit(val gasto: Gasto) : GastoEvent
+    data object ClearMessages : GastoEvent
+}
